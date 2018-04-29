bcff3116862c6c1b47e230bffdcc790ae6d98996
gcsumike <michael.new@bobcats.gcsu.edu>
2018-04-29 17:31:05 -0400

    Adding app Please enter the commit message for your changes. Lines starting

---
+++ app/models/user.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+var UserSchema = new Schema({
+	username: {type: String, lowercase: true, required: true, unique: true},
+	password: {type: String, required: true, },
+	email: {type: String, required: true, lowercase: true, unique: true}
+});
+
+
+UserSchema.methods.comparePassword = function(password){
+	return (password == this.password);
+} 
+
+
+module.exports = mongoose.model('User', UserSchema);
